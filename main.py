af612ceca55fd7d978d478b6b4ea42c9ce642dc7
Georgegoodey <george@goodey.co.uk>
2023-07-24 19:21:49 +0100

    Functional Graph traversal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 import numpy as np
-import networkx as nx
 
 class Ant:
 
@@ -32,15 +31,12 @@ class Ant:
         i = self.node
         probs = []
         nodes = self.mat[i]
-        # validNodes = list(filter(lambda x: x != 0, nodes))
-        # eta = list(map(lambda x: 1/x, validNodes))
-        # print(eta)
         for n in range(len(nodes)):
             prob = self.probabilityIJ(i,n,tau,self.mat,alpha,beta)
             probs.append(prob)
         return random.choices(range(len(probs)),weights=probs,k=1)[0]
     
-    def move(self,tau,alpha,beta):
+    def move(self,tau,alpha,beta) -> None:
         while(self.node != self.end):
             newNode = self.nextNode(tau,alpha,beta)
             self.route.append(newNode)
@@ -55,31 +51,31 @@ class Ant:
 
 def main() -> None:
     # adjMat  =  [[0,1,2,0],[1,0,0,1],[2,0,0,2],[0,1,2,0]]
-    adjMat = np.array([
+    adjMat = [
         # 0   1   2   3   4   5   6   7   8   9
         [0,  3,  5,  0,  0,  0,  0,  0,  0,  0],  # Vertex 0
         [0,  0,  2,  7,  0,  0,  0,  0,  0,  4],  # Vertex 1
         [0,  0,  0,  0,  4,  0,  0,  2,  2,  0],  # Vertex 2
-        [0,  0,  0,  1,  6,  0,  8,  0,  0,  8],  # Vertex 3
+        [0,  0,  0,  0,  6,  0,  8,  0,  0,  8],  # Vertex 3
         [8,  3,  0,  0,  0,  1,  9,  0,  6,  0],  # Vertex 4
         [0,  2,  0,  0,  1,  0,  2,  0,  0,  0],  # Vertex 5
         [0,  0,  3,  0,  0,  5,  0,  0,  1,  5],  # Vertex 6
         [4,  0,  1,  0,  2,  0,  0,  0,  3,  0],  # Vertex 7
         [0,  2,  0,  5,  0,  7,  0,  4,  0,  4],  # Vertex 8
         [1,  0,  0,  1,  5,  0,  3,  0,  2,  0],  # Vertex 9
-    ])
-    print(np.shape(adjMat))
+    ]
     tau  = np.ones(np.shape(adjMat))
     print(tau)
     alpha = 1
     beta = 2
-    evaporationCoeff = 0.2
-    q = 0.5
+    evaporationCoeff = 0.1
+    q = 1
     antCount = int(input("How many ants do you want to simulate: "))
     iterations = int(input("How many iterations do you want to simulate: "))
     start = int(input("Enter start node: "))
     end = int(input("Enter goal node: "))
     bestCost = float("inf")
+    bestRoute = []
     ants = []
     for i in range(iterations):
         ants = []
@@ -88,16 +84,15 @@ def main() -> None:
             ants.append(Ant(start,end,adjMat))
         for ant in ants:
             ant.move(tau,alpha,beta)
+            if(ant.cost < bestCost):
+                bestCost = ant.cost
+                bestRoute = ant.route
             for r in range(len(ant.route)-1):
                 tauChange[ant.route[r]][ant.route[r+1]] += q / ant.cost
         tau *= (1-evaporationCoeff)
-        tau += tauChange
-        print(tau)
-    for a in ants:
-        if(ant.cost <= bestCost):
-            print("Best Route: "+str(ant.route))
-            print("Cost of: "+str(ant.cost))
-            break
+        tau += tauChange / antCount
+    print("Best Route: "+str(bestRoute))
+    print("Cost of: "+str(bestCost))
 
 if __name__ == "__main__":
     main()
